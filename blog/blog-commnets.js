e0377b185aa1a91027fd8e3f873b373da037f160
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-06 13:39:13 -0500

    User can modify blog

--- blog/blog-commnets.js
+++ blog/blog-commnets.js
@@ -2,16 +2,16 @@ const express = require('express');
 const Comment = require('../data/db.js');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	Comment.findPostComments(postId);
-	console
-		.log(postId)
-		.then((postId) => {
-			res.status(200).json(postId);
-		})
-		.catch((err) => {
-			res.status(500).json({ message: 'Error getting comment' });
-		});
-});
+// router.get('/', (req, res) => {
+// 	Comment.findPostComments(postId);
+// 	console
+// 		.log(postId)
+// 		.then((postId) => {
+// 			res.status(200).json(postId);
+// 		})
+// 		.catch((err) => {
+// 			res.status(500).json({ message: 'Error getting comment' });
+// 		});
+// });
 
 module.exports = router;
--- blog/blog-router.js
+++ blog/blog-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Blog = require('../data/db.js');
 const router = express.Router();
 
+//Get all posts
 router.get('/', (req, res) => {
 	Blog.find(req.query)
 		.then((post) => {
@@ -12,6 +13,7 @@ router.get('/', (req, res) => {
 			res.status(500).json({ message: 'Error getting posts' });
 		});
 });
+//get posts by id
 router.get('/:id', (req, res) => {
 	Blog.findById(req.params.id).then((post) => {
 		if (post) {
@@ -21,6 +23,8 @@ router.get('/:id', (req, res) => {
 		}
 	});
 });
+
+//add post to list
 router.post('/', (req, res) => {
 	Blog.insert(req.body)
 		.then((blog) => {
@@ -31,6 +35,24 @@ router.post('/', (req, res) => {
 			res.status(400).json({
 				errorMessage: 'Please provide title and contents for the post.',
 			});
+		});
+});
+
+//edit post using id
+router.put('/:id', (req, res) => {
+	const changes = req.body;
+	Blog.update(req.params.id, changes)
+		.then((blog) => {
+			if (blog) {
+				res.status(200).json(blog);
+			} else {
+				res.status(400).json({ message: 'blog cant be found' });
+			}
+		})
+		.catch((error) => {
+			res
+				.status(404)
+				.json({ message: 'The post with the specified ID does not exist.' });
 		});
 });
 
