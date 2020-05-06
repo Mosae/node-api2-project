8a69567be8f7ec5e8769d32e371b8ea415861c37
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-06 01:54:25 -0500

    can get post by id

--- blog/blog-router.js
+++ blog/blog-router.js
@@ -19,5 +19,14 @@ router.get('/', (req, res) => {
 			res.status(500).json({ message: 'Error getting posts' });
 		});
 });
+router.get('/:id', (req, res) => {
+	Blog.findById(req.params.id).then((post) => {
+		if (post) {
+			res.status(200).json(post);
+		} else {
+			res.status(404).json({ message: 'Post not found' });
+		}
+	});
+});
 
 module.exports = router;
